ac6e51d84e26c65ccc1de4d229ba0809c0241367
yxj-github-437 <2457369732@qq.com>
2025-09-25 23:21:04 +0800

    add gcc prebuild

---
+++ build.sh
@@ -0,0 +1,73 @@
+#! /bin/sh
+
+BASE_DIR=$(pwd)
+GCC_VERSION=15.2.0
+BINUTILS_VERSION=2.45
+JBOS=1
+TARGET=aarch64-linux-android
+HOST=aarch64-linux-android
+
+
+#### handle commands
+while [[ $# > 0 ]]; do
+	case $1 in
+		--jobs=*)
+			[[ ${1/--jobs=/} == 0 ]] && {
+				echo "jobs value must be greater than 0. termiate" && exit 1
+			}
+			JOBS=${1/--jobs=/}
+		;;
+		--gcc-version=*)
+			GCC_VERSION=${1/--gcc-version=/}
+		;;
+		--binutils-version=*)
+			BINUTILS_VERSION=${1/--binutils-version=/}
+		;;
+		--target=*)
+			TARGET=${1/--target=/}
+		;;
+		--host=*)
+			HOST=${1/--host=/}
+		;;
+		*)
+			echo "bad command line \"$1\". termiate" && exit 1
+		;;
+	esac
+	shift
+done
+
+
+mkdir -p $BASE_DIR/src/
+
+
+## download gcc
+wget https://gcc.gnu.org/pub/gcc/releases/gcc-$GCC_VERSION/gcc-$GCC_VERSION.tar.xz -q -P /tmp/
+tar xf /tmp/gcc-$GCC_VERSION.tar.xz -C $BASE_DIR/src
+
+## download binutils
+wget http://ftp.gnu.org/gnu/binutils/binutils-$BINUTILS_VERSION.tar.xz -q -P /tmp/
+tar xf /tmp/binutils-$BINUTILS_VERSION.tar.xz -C $BASE_DIR/src
+
+
+patch -d $BASE_DIR/src/gcc-$GCC_VERSION -p1 < $BASE_DIR/patches/gcc/*.patch || exit 1
+patch -d $BASE_DIR/src/binutils-$BINUTILS_VERSION -p1 < $BASE_DIR/patches/binutils/*.patch || exit 1
+
+cd $BASE_DIR/src/gcc-$GCC_VERSION; contrib/download_prerequisites || exit 1
+for dir in bfd binutils elfcpp gas ld libctf libsframe opcodes; do
+	ln -sf $BASE_DIR/src/binutils-$BINUTILS_VERSION/$i $BASE_DIR/src/gcc-$GCC_VERSION/$i
+done
+
+
+
+## prebuild
+mkdir -p $BASE_DIR/prebuild; cd $BASE_DIR/prebuild
+../src/gcc-$GCC_VERSION/configure --host=x86_64-linux-gnu --target=$TARGET --build=x86_64-linux-gnu --enable-default-pie --enable-host-pie --enable-languages=c,c++ --with-system-zlib --with-system-zstd --with-target-system-zlib --enable-multilib --enable-multiarch \
+	--disable-tls --disable-shared --with-pic --enable-checking=release --disable-rpath --enable-new-dtags --enable-ld=default --enable-gold --disable-libssp --disable-libitm --enable-gnu-indirect-function --disable-relro --disable-werror --enable-libphobos-checking=release \
+	--enable-version-specific-runtime-libs --with-build-config=bootstrap-lto-lean --enable-link-serialization=2 --disable-vtable-verify --enable-plugin --prefix=/opt/gcc-install --with-build-sysroot=/opt/android-build/sysroot --with-sysroot=/opt/android-build/sysroot \
+	--disable-bootstrap
+make -j $JOBS
+make install
+
+
+find * in /opt/gcc-install/
+
