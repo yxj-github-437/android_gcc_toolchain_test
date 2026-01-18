19dfcec666cdc37543808fb0a8c187250b4458be
yxj-github-437 <2457369732@qq.com>
2026-01-18 22:28:36 +0800

    add gccrs in build

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ BASE_DIR=$(pwd)
 PROJECT_DIR=$(pwd)
 GCC_VERSION=15.2.0
 GCC_PATH=
+ENABLE_LANGUAGES=c,c++,fortran,d
 BINUTILS_VERSION=2.45
 ZSTD_VERSION=1.5.7
 JBOS=1
@@ -40,6 +41,9 @@ while [[ $# > 0 ]]; do
 		;;
 		--host=*)
 			HOST=${1/--host=/}
+		;;
+		--enable-languages=*)
+			ENABLE_LANGUAGES=${1/--enable-languages=/}
 		;;
 		*)
 			echo "bad command line \"$1\". termiate" && exit 1
@@ -89,6 +93,7 @@ gcc_15_patches=(
 )
 
 gcc_16_patches=(
+	0001-enable-canadian-cross-compile-for-host-android.patch
 	gcc-16-avoid-hardlink-in-Android.patch
 	gcc-16-fix-build-for-Android.patch
 )
@@ -124,7 +129,7 @@ export LD=ld.lld
 export AR=llvm-ar
 
 mkdir -p $BASE_DIR/prebuild; cd $BASE_DIR/prebuild
-../src/gcc-$GCC_VERSION/configure --host=x86_64-linux-gnu --target=$TARGET --build=x86_64-linux-gnu --enable-default-pie --enable-host-pie --enable-languages=c,c++,fortran,d --with-system-zlib --with-system-zstd --with-target-system-zlib --enable-multilib --enable-multiarch \
+../src/gcc-$GCC_VERSION/configure --host=x86_64-linux-gnu --target=$TARGET --build=x86_64-linux-gnu --enable-default-pie --enable-host-pie --enable-languages=$ENABLE_LANGUAGES --with-system-zlib --with-system-zstd --with-target-system-zlib --enable-multilib --enable-multiarch \
 	--disable-tls --disable-shared --with-pic --enable-checking=release --disable-rpath --enable-new-dtags --enable-ld=default --enable-gold --disable-libssp --disable-libitm --enable-gnu-indirect-function --disable-relro --disable-werror --enable-libphobos-checking=release \
 	--enable-version-specific-runtime-libs --with-build-config=bootstrap-lto-lean --enable-link-serialization=2 --disable-vtable-verify --enable-plugin --with-build-sysroot=/opt/android-build/sysroot --with-sysroot=/opt/android-build/sysroot \
 	--disable-bootstrap  --prefix=$PREINSTALL_DIR --with-gmp=/usr --with-mpfr=/usr --with-mpc=/usr --with-isl=/usr
@@ -139,17 +144,24 @@ CC=$HOST-gcc CFLAGS="-fPIC -O2" make lib -j $JOBS -C $BASE_DIR/src/zstd-$ZSTD_VE
 mkdir -p $PREINSTALL_DIR/$HOST/include/ && cp -r $BASE_DIR/src/zstd-$ZSTD_VERSION/lib/*.h $PREINSTALL_DIR/$HOST/include/ || exit 1
 mkdir -p $PREINSTALL_DIR/$HOST/lib/ && cp -r $BASE_DIR/src/zstd-$ZSTD_VERSION/lib/libzstd.a $PREINSTALL_DIR/$HOST/lib/ || exit 1
 
-
+cat << EOF > $PREINSTALL_DIR/$HOST/lib/libunwind.a
+INPUT(-lgcc)
+EOF
 
 cd $BASE_DIR/src/gcc-$GCC_VERSION; contrib/download_prerequisites || exit 1
 for i in `find $PROJECT_DIR/patches/gettext/ -name *.patch -type f`; do
 	patch -d $BASE_DIR/src/gcc-$GCC_VERSION/gettext -p1 < $i || exit 1
 done
 
+if [ -f $BASE_DIR/src/gcc-$GCC_VERSION/libgrust/libformat_parser/.cargo/config.toml ]; then
+	echo [target.$HOST] >> $BASE_DIR/src/gcc-$GCC_VERSION/libgrust/libformat_parser/.cargo/config.toml
+	echo linker = \"$HOST-gcc\" >> $BASE_DIR/src/gcc-$GCC_VERSION/libgrust/libformat_parser/.cargo/config.toml
+fi
+
 ## build
 mkdir -p $BASE_DIR/build; cd $BASE_DIR/build
 export gcc_cv_objdump=llvm-objdump
-../src/gcc-$GCC_VERSION/configure --host=$HOST --target=$TARGET --build=x86_64-linux-gnu --enable-default-pie --enable-host-pie --enable-languages=c,c++,fortran,d --with-system-zlib --with-system-zstd --with-target-system-zlib --enable-multilib --enable-multiarch \
+../src/gcc-$GCC_VERSION/configure --host=$HOST --target=$TARGET --build=x86_64-linux-gnu --enable-default-pie --enable-host-pie --enable-languages=$ENABLE_LANGUAGES --with-system-zlib --with-system-zstd --with-target-system-zlib --enable-multilib --enable-multiarch \
 	--disable-tls --disable-shared --with-pic --enable-checking=release --disable-rpath --enable-new-dtags --enable-ld=default --enable-gold --disable-libssp --disable-libitm --enable-gnu-indirect-function --disable-relro --disable-werror --enable-libphobos-checking=release \
 	--enable-version-specific-runtime-libs --with-build-config=bootstrap-lto-lean --enable-link-serialization=2 --disable-vtable-verify --enable-plugin --prefix=/usr --with-build-sysroot=/opt/android-build/sysroot --with-sysroot=/usr/sysroot \
 	--disable-bootstrap
